fc9fa50f70f991f735b61b5c4dd761ace0c476d5
Meowbutu <mewmewdevart@gmail.com>
2023-01-27 11:49:58 -0300

    second steps

---
+++ Day02/exercises/learning_dataTypes.py
@@ -0,0 +1,21 @@
+# Data Types : String, Integer, Float and Boolean
+
+# String
+print("Hello"[4])
+    #  H   E  L  L O
+    # [0][1][2][3][4]
+
+# Integer
+print(123 + 123)
+    # Writing large integer in python we dont use " , " we use " _ "
+    # 123,020,3010
+    # 123_020_3010
+
+# Float
+3.14159
+
+#Boolean
+    True
+    False
+
+# Float
