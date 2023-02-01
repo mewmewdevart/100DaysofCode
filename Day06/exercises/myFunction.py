2498dadfd10003e62750ff13ac4c8aeb45417dee
Meowbutu <mewmewdevart@gmail.com>
2023-01-31 21:42:24 -0300

    not finished day 06

---
+++ Day06/exercises/myFunction.py
@@ -0,0 +1,16 @@
+# print("Hello")
+# num_char = len("Hello")
+# print(num_char)
+
+def myFunction():
+    print("Hello")
+    print("Bye")
+
+# we have to execute the function
+myFunction()
+
+# the function works like
+# def my_function():
+    #do this
+    #then do this
+    #finally do
