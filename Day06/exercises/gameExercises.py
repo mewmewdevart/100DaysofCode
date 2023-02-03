9419de32acb04b6cbcb37d76c3680462600746da
Meowbutu <mewmewdevart@gmail.com>
2023-02-03 20:16:25 -0300

    day06 - hello my friend

---
+++ Day06/exercises/gameExercises.py
@@ -0,0 +1,25 @@
+# My solution to this first challenge: Draw a square on the screen
+# link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    turn_left()
+
+# Draw one square
+move()
+move()
+turn_right()
+turn_left()
+move()
+move()
+turn_right()
+turn_left()
+move()
+move()
+turn_right()
+turn_left()
+move()
+move()
+turn_right()
+turn_left()
