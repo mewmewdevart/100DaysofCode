a77e857dea512184e6b51ae3e49266e4fe5f6a37
Meowbutu <mewmewdevart@gmail.com>
2023-02-03 21:00:33 -0300

    more exercises

---
+++ Day06/exercises/identation.py
@@ -0,0 +1,12 @@
+def my_functions():
+    print("Hello")
+    print("Manteiga")
+print("Nao é manteiga")
+
+def my_function():
+    if sky == 'clear':
+        print(blue)
+    elif sky == "cloudy"
+        print("grey")
+    print("hello")
+print("world")
