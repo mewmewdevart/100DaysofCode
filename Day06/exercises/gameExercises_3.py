3f90b6761cf2ee194e9c5879f587cac2fbc15df0
Meowbutu <mewmewdevart@gmail.com>
2023-02-03 23:13:36 -0300

    upp more exercises

---
+++ Day06/exercises/gameExercises_3.py
@@ -0,0 +1,23 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
+
+# or
+# while not at_goal()
+while at_goal() != True:
+    jump()
---
+++ Day06/exercises/whileAndFor.py
@@ -0,0 +1,15 @@
+
+# for loops
+for item in list_of_items:
+    #do something to each item
+
+for number in range(a, b):
+    print(number)
+
+# while loops
+while something_is_true:
+    #do something repeatedly
+
+while this_is_true > 0:
+    print("it is true")
+    this_is_true -= 1
