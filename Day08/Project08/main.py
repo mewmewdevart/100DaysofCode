91850bacc67ab6071541f191ad41d56250b1c26a
Meowbutu <mewmewdevart@gmail.com>
2023-02-11 23:19:15 -0300

    refactor and fix : add decode part of the project

--- Day08/Project08/main.py
+++ Day08/Project08/main.py
@@ -1,19 +1,46 @@
 from word_alphabet import alphabet
 
+# ASCII art
+print('''
+        ⠀⢠⣶⣿⣿⣗⡢⠀⠀⠀⠀⠀⠀⢤⣒⣿⣿⣷⣆⠀⠀
+        ⠀⠋⠉⠉⠙⠻⣿⣷⡄⠀⠀⠀⣴⣿⠿⠛⠉⠉⠉⠃⠀
+        ⠀⠀⢀⡠⢤⣠⣀⡹⡄⠀⠀⠀⡞⣁⣤⣠⠤⡀⠀⠀⠀
+        ⢐⡤⢾⣿⣿⢿⣿⡿⠀⠀⠀⠀⠸⣿⣿⢿⣿⣾⠦⣌⠀
+        ⠁⠀⠀⠀⠉⠈⠀⠀⣸⠀⠀⢰⡀⠀⠈⠈⠀⠀⠀⠀⠁
+        ⠀⠀⠀⠀⠀⠀⣀⡔⢹⠀⠀⢸⠳⡄⡀⠀⠀⠀⠀⠀⠀
+        ⠸⡦⣤⠤⠒⠋⠘⢠⡸⣀⣀⡸⣠⠘⠉⠓⠠⣤⢤⡞⠀
+        ⠀⢹⡜⢷⣄⠀⣀⣀⣾⡶⢶⣷⣄⣀⡀⢀⣴⢏⡾⠁⠀
+        ⠀⠀⠹⡮⡛⠛⠛⠻⠿⠥⠤⠽⠿⠛⠛⠛⣣⡾⠁⠀⠀
+        ⠀⠀⠀⠙⢄⠁⠀⠀⠀⣄⣀⡄⠀⠀⠀⢁⠞⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠂⠀⠀⠀⢸⣿⠀⠀⠀⠠⠂⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+        ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀We are anonymous decrypts!
+''')
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(string_text, numbers_shift):
+def en_and_decrypt(string_text, numbers_shift):
     cipher_text = ""
+    check = 0
     for letter in string_text:
         position = alphabet.index(letter)
-        new_position = position + numbers_shift
+        if direction == "encode" or direction == "ENCODE":
+            new_position = position + numbers_shift
+            check += 1
+        else:
+            new_position = position - numbers_shift
         new_letter = alphabet[new_position]
         cipher_text += new_letter
-    print(f"The encode text is {cipher_text}")
+    if check != 0:
+        print(f"The encode text is {cipher_text}")
+    else:
+        print(f"The decode text is {cipher_text}")
 
 
-encrypt(text, shift)
+if direction == "ENCODE" or direction == "encode" or direction == "DECODE" or direction == "decode":
+    en_and_decrypt(text, shift)
+else:
+    print("Please, enter a true words for to be converted!")
