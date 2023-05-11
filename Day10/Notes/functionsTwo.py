6bf8bc552c2ef00a81341cf7ac88e9e0a72c7e41
Meowbutu <mewmewdevart@gmail.com>
2023-05-10 21:26:36 -0300

    to be continue

---
+++ Day10/Notes/functionsTwo.py
@@ -0,0 +1,7 @@
+# Functions with outputs
+def format_name(f_name, l_name):
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return first_name, last_name
+
+print(format_name("larissa", "cristina"))
