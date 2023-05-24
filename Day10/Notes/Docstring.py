f70f179b3bb31df40d748d9781389c09adceccd9
Meowbutu <mewmewdevart@gmail.com>
2023-05-23 22:55:51 -0300

    some notes and finished day10

---
+++ Day10/Notes/Docstring.py
@@ -0,0 +1,9 @@
+
+# Functions with outputs
+def format_name(f_name, l_name):
+    """"This is my docstring : It take a first and last name and format it to return the title case version of Game!""""
+    first_name = f_name.title()
+    last_name = l_name.title()
+    return first_name, last_name
+
+print(format_name("larissa", "cristina"))
